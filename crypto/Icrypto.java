66e41ad96da72ccab51e9aaad3d52c0e1b15101d
Batcharou <127033070+Batcharou@users.noreply.github.com>
2023-03-22 12:25:03 +0000

    Add files via upload

--- crypto/Icrypto.java
+++ crypto/Icrypto.java
@@ -1,22 +1,19 @@
-package crypto;
-/**
- * @author DIENG Khaly
- */
+import java.security.NoSuchAlgorithmException;
+
 import javax.crypto.*;
 
-/**
- * Cette interface définit une méthode générique pour générer une clé secrète
- * utilisée pour le chiffrement et le déchiffrement de données.
- */
-public interface Icrypto {
-    
+public interface Icrypto{
     /**
-     * Cette méthode permet de générer une clé secrète pour l'algorithme de chiffrement spécifié
-     *
-     * @param algo l'algorithme de chiffrement pour lequel la clé doit être générée
-     * @param keySize la taille de la clé
-     * @return la clé secrète générée, ou null si une erreur s'est produite
+     * 
+     * @param algo
+     * @param keySize
+     * @return la cle
+     * @throws NoSuchAlgorithmException
      */
-    public SecretKey genKey(String algo, int keySize);
-    //////
+    public SecretKey genKey(String algo, int keySize) throws NoSuchAlgorithmException;
+
+    //public String saveKey(SecretKey key, String filePath);
+
+    //public SecretKey getKey(String filePath);
 }
+
